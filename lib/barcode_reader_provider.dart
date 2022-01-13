7b4220ecd1f983539f37724c6f1b151989f1cfcf
jon_s <tw-jon_s@towa-gifu.co.jp>
2022-01-13 09:58:40 +0900

    flutter qr bar scannerテスト追加

--- lib/barcode_reader_provider.dart
+++ lib/barcode_reader_provider.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'barcode_readers/bluetooth_barcode_scanner_page.dart';
-import 'barcode_readers/flutter_barcode_scanner.dart';
 import 'barcode_readers/fast_barcode_scanner_screen.dart';
+import 'barcode_readers/flutter_barcode_scanner.dart';
+import 'barcode_readers/flutter_qr_bar_scanner_page.dart';
 
 class BarcodeReaderProvider {
   const BarcodeReaderProvider();
@@ -12,6 +13,7 @@ class BarcodeReaderProvider {
     return {
       'flutter': (context) => scanBarcodeWithFlutterBarcodeScanner(),
       'fast': (context) => _readWithFastBarcode(context),
+      'QR Bar': (context) => _readWithQRBarScanner(context),
       'keyboard': (context) => _readWithBluetoothReader(context)
     }.entries;
   }
@@ -20,6 +22,13 @@ class BarcodeReaderProvider {
     return Navigator.of(context)
         .push<String?>(MaterialPageRoute(builder: (context) {
       return const FastBarcodeScannerScreen();
+    }));
+  }
+
+  Future<String?> _readWithQRBarScanner(BuildContext context) async {
+    return Navigator.of(context)
+        .push<String?>(MaterialPageRoute(builder: (context) {
+      return const FlutterQrBarScannerPage();
     }));
   }
 
---
+++ lib/barcode_readers/flutter_qr_bar_scanner_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_qr_bar_scanner/flutter_qr_bar_scanner.dart';
+import 'package:flutter_qr_bar_scanner/qr_bar_scanner_camera.dart';
+
+class FlutterQrBarScannerPage extends StatefulWidget {
+  const FlutterQrBarScannerPage();
+
+  _FlutterQrBarScannerPageState createState() =>
+      _FlutterQrBarScannerPageState();
+}
+
+class _FlutterQrBarScannerPageState extends State<FlutterQrBarScannerPage> {
+  bool _barcodeRead = false;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() => _barcodeRead = false);
+  }
+
+  void _scanCode(String? code) {
+    if (!_barcodeRead) {
+      setState(() => _barcodeRead = true);
+      Navigator.pop(context, code);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        onWillPop: () async {
+          return true;
+        },
+        child: Scaffold(
+          appBar: AppBar(title: Text('Flutter QR Bar Scanner')),
+          body: Center(
+            child: SizedBox(
+              height: 1000,
+              width: 500,
+              child: QRBarScannerCamera(
+                  formats: [BarcodeFormats.QR_CODE, BarcodeFormats.CODE_128],
+                  onError: (context, error) => Text(
+                        error.toString(),
+                        style: TextStyle(color: Colors.red),
+                      ),
+                  qrCodeCallback: _scanCode),
+            ),
+          ),
+        ));
+  }
+}
